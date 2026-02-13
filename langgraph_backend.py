c2b0d422c468e739500cdf3e3104bf180ee2e8f5
Ayush-977 <ayushmishra1365@gmail.com>
2026-02-13 15:09:22 +0530

    some changes

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -42,16 +42,6 @@ def calculator(a:float,b:float,operation:str) -> dict:
     return {"error": str(e)}
 
 
-@tool
-def get_stock_price(symbol: str) -> dict:
-  """
-  Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA')
-  using Alpha Vantage with API key in the URL.
-  """
-  url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=ALPHA_VANTAGE_API_KEY"
-  r = requests.get(url)
-  return r.json()
-
 
 
 # --- 1. Setup Database ---
